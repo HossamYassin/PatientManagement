c060994997df802f16dc403e730c5b648b679121
Hossam yassin <hyassin.c@tetco.sa>
2025-02-22 01:02:02 +0200

    enhancments

--- PatientManagement.Application/Contracts/IAppointmentRepository.cs
+++ PatientManagement.Application/Contracts/IAppointmentRepository.cs
@@ -5,7 +5,7 @@ namespace PatientManagement.Application.Contracts
     public interface IAppointmentRepository
     {
         Task<List<Appointment>> GetByPatientIdAsync(int patientId);
-        Task<Appointment> GetByIdAsync(int id);
+        Task<Appointment?> GetByIdAsync(int id);
         Task<int> AddAsync(Appointment appointment);
     }
 }
